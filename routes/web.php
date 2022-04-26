bb4ba430fd1d7dae4bd00df73da5c33f20937488
vitorlfaria <vitorlacerdafaria7@gmail.com>
2022-04-26 11:53:21 -0300

    Laravel inicial

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\PagesController;
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PagesController::class, 'index'])->name('home');
-
-Route::get('/shop', [ProductController::class, 'index'])->name('shop');
-Route::get('/shop/{id}', [ProductController::class, 'show'])->name('product');
-
-Route::get('/cart', [CartController::class, 'cart'])->name('cart');
-Route::get('/add-to-cart/{id}', [CartController::class, 'addToCart'])->name('add.to.cart');Route::get('/delete-from-cart/{id}', [CartController::class, 'delete'])->name('delete.from.cart');
+Route::get('/', function () {
+    return view('welcome');
+});
