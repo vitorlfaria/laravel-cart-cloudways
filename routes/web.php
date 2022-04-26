a0d98e9dd1674ffcb37ea2ca8f18b6008467af37
vitorlfaria <vitorlacerdafaria7@gmail.com>
2022-04-26 12:00:27 -0300

    Controllers, Models e rotas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PagesController::class, 'index'])->name('home');
+Route::get('/shop', [ProductController::class, 'index'])->name('shop');
+Route::get('/shop/{id}', [ProductController::class, 'show'])->name('product');
+Route::get('/cart', [CartController::class, 'cart'])->name('car');
